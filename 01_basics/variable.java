87b9d9ea3558c7e1afeb1617c7433cd36e5784b4
Pradeep8253 <89512667+Pradeep8253@users.noreply.github.com>
2024-11-14 22:57:09 +0530

    1st module completed

---
+++ 01_basics/variable.java
@@ -0,0 +1,15 @@
+public class variable {
+    public static void main (String args []){
+        int a = 10 ;
+        int b = 5 ;
+     System.out.println("a: " + a + ", b: " + b);
+
+     String name = "Shivam";
+     System.out.println(name);
+
+     a = 50 ;
+
+     System.out.println(a);
+
+    }
+}
